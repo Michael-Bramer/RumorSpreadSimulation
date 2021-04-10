a1f22d15a538335648f6c8dbc8af2225437931e7
Michael A Bramer <mabram02@louisville.edu>
2021-04-09 21:12:19 -0400

    Completed Build

--- Program.cs
+++ Program.cs
@@ -40,32 +40,102 @@ namespace RumorSpreadSimulation
 {
     class Program
     {
-        private static int SuspendSpreadLimit = 2;
-        private static int NumberOfGuest = 100;
         private static float ProbabilityOfSpreadingTheRumor = 0.5f;
 
         struct Person
         {
+            public int Id;
             public Boolean HasHeardRumor;
-            public int InformedOfRumorCount;
+            public int InformedByCount;
+            public int FirstInformedBy;
+            public int SecondInformedBy;
             public float ProbabilityOfSpreadingTheRumor;
         };
 
+        struct CommunitySpreadStatistics
+        {
+            public int MinutesElapsed;
+            public int CountAwareOfRumor;
+            public float PercentAwareOfRumor;
+        }
 
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool exit_program = false;
+
+            //Program Control Flow 
+            while (!exit_program)
+            {
+                switch (DisplayMainMenu())
+                {
+                    case 1: //Display Prompt
+                        //DisplayPrompt();
+                        break;
+                    case 2: //Run Simulation for 100 Attendees
+                        SimulateAverageRumorSpread(10, 40, 100);
+                        break;
+                    case 3: //Run Simulation for 1000 Attendees
+                        SimulateAverageRumorSpread(10, 40, 1000);
+                        break;
+                    case 4: //Run Simulation for 10000 Attendees
+                        SimulateAverageRumorSpread(10, 40, 10000);
+                        break;
+                    case 5: //Run Simulation for Averages of  N = {100, 1,000, 10,000} Attendees
+                        //SimulateMultipleAverageRumorSpread(1, 100);
+                        break;
+                    case 6: //Exit Simulation
+                        exit_program = true;
+                        break;
+                    default:
+                        PressEnterToReturn("ERROR: Please Provide a Valid Numerical Input and Try Again!");
+                        break;
+                }
+
+                Console.Clear();
+            }
         }
 
-        private static Person InitializePerson( Boolean HasHeardRumor, int InformedOfRumorCount, float ProbabilityOfSpread)
+        #region User Interface Functions
+         public static int DisplayMainMenu()
         {
-            Person Guest;
-            Guest.InformedOfRumorCount           = InformedOfRumorCount;
-            Guest.HasHeardRumor                  = HasHeardRumor;
-            Guest.ProbabilityOfSpreadingTheRumor = ProbabilityOfSpread;
-            return Guest;
+            int selection_code = 0;
+            Console.WriteLine("|=====================================================|");
+            Console.WriteLine("|       Welcome to the Rumor Spread Simulation!       |");
+            Console.WriteLine("|=====================================================|\n\n");
+            Console.WriteLine("Please Select an Action to Perform:");
+            Console.WriteLine("\n\t1.] Display Prompt");
+            Console.WriteLine("\t2.] Run Simulation for 100 Attendees");
+            Console.WriteLine("\t3.] Run Simulation for 1000 Attendees");
+            Console.WriteLine("\t4.] Run Simulation for 10000 Attendees");
+            Console.WriteLine("\t5.] Run Simulation for Averages of  N = {100, 1,000, 10,000} Attendees");
+            Console.WriteLine("\t6.] Exit Simulation");
+
+            Console.Write("\nInput: ");
+            string Input = Console.ReadLine();
+
+            try
+            {
+                selection_code = Int32.Parse(Input);
+            }
+            catch (FormatException)
+            {
+                //Console.WriteLine("Please Provide a Valid Numerical Input and Try Again!\n");
+                selection_code = -1;
+            }
+            return selection_code;
         }
 
+        public static void PressEnterToReturn(string DisplayText)
+        {
+            Console.WriteLine("\n" + DisplayText + "\n");
+            string DummyInput = Console.ReadLine();
+        }
+
+        #endregion
+
+
+        #region Utility Functions
         //Refrenced From: https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
         private static int[] FisherYatesShuffle(int[] arr, int size)
         {
@@ -90,10 +160,211 @@ namespace RumorSpreadSimulation
             return arr;
         }
 
+        private static void SimulateAverageRumorSpread(int NumberOfRuns, int DurationInMinutes, int NumberOfParticipants)
+        {
+            CommunitySpreadStatistics[,] TraceFileData = new CommunitySpreadStatistics[NumberOfRuns + 1, DurationInMinutes];
+            CommunitySpreadStatistics[] PartyTrace     = new CommunitySpreadStatistics[DurationInMinutes];
+            for (int RunCount = 0; RunCount < NumberOfRuns; RunCount++)
+            {
+                PartyTrace = SimulateParty(DurationInMinutes, NumberOfParticipants, true);
+                for(int i =0; i < DurationInMinutes; i++)
+                {
+                    TraceFileData[RunCount, i] = PartyTrace[i];
 
+                    //Populate The Average Structure
+                    TraceFileData[NumberOfRuns, i].MinutesElapsed = i;
+                    TraceFileData[NumberOfRuns, i].PercentAwareOfRumor += PartyTrace[i].PercentAwareOfRumor;
+                    TraceFileData[NumberOfRuns, i].CountAwareOfRumor   += PartyTrace[i].CountAwareOfRumor;
+                }
+            }
 
+            //Calculate the Averages in the Index of the Structure
+            for(int i = 0; i < DurationInMinutes; i++)
+            {
+                TraceFileData[NumberOfRuns, i].PercentAwareOfRumor = TraceFileData[NumberOfRuns, i].PercentAwareOfRumor / NumberOfRuns;
+                TraceFileData[NumberOfRuns, i].CountAwareOfRumor   = TraceFileData[NumberOfRuns, i].CountAwareOfRumor   / NumberOfRuns;
+            }
 
+            GenerateFile(TraceFileData, NumberOfRuns, NumberOfParticipants, DurationInMinutes);
+        }
 
+        private static void GenerateFile(CommunitySpreadStatistics[,] TraceFile, int NumberOfRuns, int NumberOfParticipants, int DurationInMinutes)
+        {
+            string path = @"Rumor_Spread_Simulation_for_N_Equals_" + NumberOfParticipants.ToString() + @".txt";
+            try
+            {
+                // Create the file, or overwrite if the file exists.
+                using (FileStream fs = File.Create(path))
+                {
+                    byte[] infoh = new UTF8Encoding(true).GetBytes("Minutes_Elapsed");
+                    fs.Write(infoh, 0, infoh.Length);
+
+                    for(int i = 0; i < NumberOfRuns + 1; i++)
+                    {
+                        if(i == NumberOfRuns)
+                        {
+                            infoh = new UTF8Encoding(true).GetBytes("," + "Average_Percent_Aware" + "\n");
+                        }
+                        else
+                        {
+                            infoh = new UTF8Encoding(true).GetBytes("," + "Run_#_" + (i+1).ToString() + "_Percent_Aware" );
+                        }
+                        fs.Write(infoh, 0, infoh.Length);
+                    }
+
+                   for(int i = 0; i < DurationInMinutes; i++)
+                   {
+                        byte[] info = new UTF8Encoding(true).GetBytes(TraceFile[0, i].MinutesElapsed.ToString());
+                        fs.Write(info, 0, info.Length);
+                        for (int j = 0; j < NumberOfRuns + 1; j++)
+                        {
+                            info = new UTF8Encoding(true).GetBytes("," + TraceFile[j, i].PercentAwareOfRumor.ToString());
+                            fs.Write(info, 0, info.Length);
+                        }
+                        info = new UTF8Encoding(true).GetBytes("\n");
+                        fs.Write(info, 0, info.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static CommunitySpreadStatistics[] SimulateParty(int DurationInMinutes, int NumberOfGuests, Boolean OnlyCountUniqueSpread)
+        {
+            //Initialize The Trace Array
+            CommunitySpreadStatistics[] PartyTrace = new CommunitySpreadStatistics[DurationInMinutes];
+
+            //Initialize Guests and Pairing List
+            Person[] GuestList = new Person[NumberOfGuests];
+            int[] PairingList = new int[NumberOfGuests];
+
+            for (int i = 0; i < NumberOfGuests; i++)
+            {
+                //Generate Guest
+                if (i == 0)
+                {
+                    GuestList[i].HasHeardRumor = true;
+                }
+                else
+                {
+                    GuestList[i].HasHeardRumor = false;
+                }
+
+                GuestList[i].FirstInformedBy  = -1;
+                GuestList[i].SecondInformedBy = -1;
+                GuestList[i].InformedByCount  = 0;
+                GuestList[i].ProbabilityOfSpreadingTheRumor = 0.5f;
+                GuestList[i].Id = i;
+
+                //Add Guest ID to Pairing List
+                PairingList[i] = i;
+            }
+
+            //Simulate Party
+            for (int TimeElapsed = 0; TimeElapsed < DurationInMinutes; TimeElapsed++)
+            {
+                PairingList = FisherYatesShuffle(PairingList, PairingList.Length);
+                PartyTrace[TimeElapsed].MinutesElapsed = TimeElapsed + 1;
+
+                //Assume that only an Even Number of Guests are Present
+                for (int i = 1; i < NumberOfGuests; i += 2 )
+                {
+                    int PersonAIndex = i - 1;
+                    int PersonBIndex = i;
+                    Person PersonA = GuestList[PairingList[PersonAIndex]];
+                    Person PersonB = GuestList[PairingList[PersonBIndex]];
+                    Boolean PersonAWantsToTellRumor = WillSpreadRumor(GuestList[i - 1]);
+                    Boolean PersonBWantsToTellRumor = WillSpreadRumor(GuestList[i]);
+
+                    //Assume once one person brings the rumor it discourages their conversation partner from bringing it up
+                    if(PersonAWantsToTellRumor && PersonBWantsToTellRumor)
+                    {
+                        Random r = new Random();
+                        if (r.NextDouble() < 0.5)
+                        {
+                            PersonAWantsToTellRumor = false;
+                        }
+                        else
+                        {
+                            PersonBWantsToTellRumor = false;
+                        }
+                    }
+
+                    //Person A Tells Person B
+                    if (PersonAWantsToTellRumor)
+                    {
+                        if (OnlyCountUniqueSpread && PersonB.FirstInformedBy != PersonA.Id)
+                        {
+                            PersonB.InformedByCount += 1;
+                            PersonB.HasHeardRumor   = true;
+                            PersonB.FirstInformedBy = PersonA.Id;
+
+                        }
+                        else
+                        {
+                            PersonB.InformedByCount += 1;
+                            PersonB.HasHeardRumor = true;
+                            PersonB.FirstInformedBy = PersonA.Id;
+                        }
+                    }
+
+                    //Person B Tells Person A
+                    if (PersonBWantsToTellRumor)
+                    {
+                        if (OnlyCountUniqueSpread && PersonA.FirstInformedBy != PersonB.Id)
+                        {
+                            PersonA.InformedByCount += 1;
+                            PersonA.HasHeardRumor = true;
+                            PersonA.FirstInformedBy = PersonB.Id;
+
+                        }
+                        else
+                        {
+                            PersonB.InformedByCount += 1;
+                            PersonB.HasHeardRumor = true;
+                            PersonB.FirstInformedBy = PersonB.Id;
+                        }
+
+                    } 
+                    
+                    //Update Trace and Record Changes
+                    if(PersonA.HasHeardRumor == true)
+                    {
+                        PartyTrace[TimeElapsed].CountAwareOfRumor += 1;
+
+                    }
+                    GuestList[PairingList[PersonAIndex]] = PersonA;
+
+                    if (PersonB.HasHeardRumor == true)
+                    {
+                        PartyTrace[TimeElapsed].CountAwareOfRumor += 1;
+                    }
+                    GuestList[PairingList[PersonBIndex]] = PersonB;
+
+                }
+
+                PartyTrace[TimeElapsed].PercentAwareOfRumor = (float)PartyTrace[TimeElapsed].CountAwareOfRumor / NumberOfGuests;
+            }
+
+            return PartyTrace;
+        }
+
+        private static Boolean WillSpreadRumor(Person person)
+        {
+            Random r = new Random();
+            if (person.HasHeardRumor == true && r.NextDouble() < person.ProbabilityOfSpreadingTheRumor && person.InformedByCount < 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion        
 
     }
 }
